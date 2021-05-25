6f9e9a56bee8c00dea4630c6de87d51916e0458d
Your Name <you@example.com>
2021-05-25 08:31:46 +0300

    template parameters added: title, mainTitle. Output for cleaning former files in dist

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,11 +7,23 @@ module.exports = {
     // kuri faila paims webpack kaip pagrindini
     main: path.resolve(__dirname, "./src/app.js"),
   },
-  output: {},
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
   module: {},
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/html/template.html",
+      templateParameters: {
+        title: "Pakeiciau title per dev.js",
+        mainTitle: "Pakeiciau mainTitle per dev.js",
+      },
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false,
+      },
     }),
   ],
 };
